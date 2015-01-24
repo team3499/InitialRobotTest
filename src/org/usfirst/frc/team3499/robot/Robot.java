dad6eb6a8fd56cf76ac287b6294af0e1a3826a42
bturnbull <github@brianturnbull.com>
2015-01-24 15:36:52 -0500

    Convert Jaguars to PWM control

--- src/org/usfirst/frc/team3499/robot/Robot.java
+++ src/org/usfirst/frc/team3499/robot/Robot.java
@@ -5,8 +5,9 @@ import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
-import edu.wpi.first.wpilibj.CANJaguar;
+import edu.wpi.first.wpilibj.Jaguar;
 import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.RobotDrive.MotorType;
 import edu.wpi.first.wpilibj.Joystick;
 import org.usfirst.frc.team3499.robot.commands.BlinkLedCommand;
 import org.usfirst.frc.team3499.robot.subsystems.LedSubsystem;
@@ -22,10 +23,10 @@ public class Robot extends IterativeRobot {
 
     public static final LedSubsystem ledSubsystem = new LedSubsystem();
 
-    public static CANJaguar motor1;
-    public static CANJaguar motor2;
-    public static CANJaguar motor3;
-    public static CANJaguar motor4;
+    public static Jaguar motor1;
+    public static Jaguar motor2;
+    public static Jaguar motor3;
+    public static Jaguar motor4;
     public static RobotDrive robotDrive;
     public static Joystick joystick;
     public static OI oi;
@@ -40,16 +41,18 @@ public class Robot extends IterativeRobot {
         oi = new OI();
         // instantiate the command used for the autonomous period
         // autonomousCommand = new BlinkLedCommand();
-        try {
-            motor1 = new CANJaguar(RobotMap.driveMotorLFCanID);   // Front Left
-            motor2 = new CANJaguar(RobotMap.driveMotorLRCanID);   // Rear Left
-            motor3 = new CANJaguar(RobotMap.driveMotorRFCanID);   // Front Right
-            motor4 = new CANJaguar(RobotMap.driveMotorRRCanID);   // Rear Right
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
+        
+        motor1 = new Jaguar(RobotMap.driveMotorLFCanID);   // Front Left
+        motor2 = new Jaguar(RobotMap.driveMotorLRCanID);   // Rear Left
+        motor3 = new Jaguar(RobotMap.driveMotorRFCanID);   // Front Right
+        motor4 = new Jaguar(RobotMap.driveMotorRRCanID);   // Rear Right
 
         robotDrive = new RobotDrive(motor1, motor2, motor3, motor4);
+        robotDrive.setInvertedMotor(MotorType.kFrontLeft, true);
+        robotDrive.setInvertedMotor(MotorType.kRearLeft, true);
+        robotDrive.setInvertedMotor(MotorType.kFrontRight, true);
+        robotDrive.setInvertedMotor(MotorType.kRearRight, true);
+        
         joystick = new Joystick(RobotMap.controlStickPort);
     }
 
@@ -91,9 +94,6 @@ public class Robot extends IterativeRobot {
     public void teleopPeriodic() {
         Scheduler.getInstance().run();
 
-        // DANGER, Will Robinson! I *think* arcadeDrive will set the motors on each
-        // side to counter counter rotate, but I'm not certain until this code is
-        // tested WITHOUT a gearbox attached.
         robotDrive.arcadeDrive(joystick);
     }
 
